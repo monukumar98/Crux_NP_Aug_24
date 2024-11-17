52e1d0f2c7b13c2e79ead2a2940245b3fde9a0de
monukumar98 <monu012as@gmail.com>
2024-11-17 16:46:31 +0530

    String Qps

--- Lec15/Finding_CB_Numbers.java
+++ Lec15/Finding_CB_Numbers.java
@@ -5,22 +5,34 @@ public class Finding_CB_Numbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s = "81615";
-printallsubstring(s);
+		printallsubstring(s);
 	}
 
 	public static void printallsubstring(String s) {
 		int c = 0;
+		boolean[] visited = new boolean[s.length()];
 		for (int len = 1; len <= s.length(); len++) {
 			for (int j = len; j <= s.length(); j++) {
 				int i = j - len;
 				long x = Long.parseLong(s.substring(i, j));
-				if (isCB_Numbers(x) == true) {
+				if (isCB_Numbers(x) && isvisited(visited, i, j - 1)) {
 					c++;
+					for (int k = i; k < j; k++) {
+						visited[k] = true;
+					}
 				}
 
 			}
 		}
 		System.out.println(c);
+	}
+	public static boolean isvisited(boolean[] visited, int si, int ei) {
+		for (int i = si; i <= ei; i++) {
+			if (visited[i] == true) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public static boolean isCB_Numbers(long nums) {
